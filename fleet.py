b990b29d76c491cf0267714818b681aa51f9a693
Josh Courtney <joshrcourtney@gmail.com>
2021-01-18 17:24:28 -0600

    Refactor Fleet class

--- fleet.py
+++ fleet.py
@@ -1,13 +1,17 @@
 class Fleet(list):
-    def __init__(self, count, car):
+    def __init__(self, car):
         super().__init__()
 
-        self.count = count
-        self.car = car
+        self._car = car
+        self._car_count = 8
 
         self._create_fleet()
 
     # PRIVATE METHODS
     def _create_fleet(self):
-        for _ in range(self.count):
-            self.append(self.car())
+        for _ in range(self._car_count):
+            self.append(self._car())
+
+    # PRIVATE METHODS
+    def update_car_count(self, count):
+        self._car_count = count
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     game = Game(
         GUI(Screen()),
         Player,
-        Fleet(8, Car),
+        Fleet(Car),
         Scoreboard()
     )
 
